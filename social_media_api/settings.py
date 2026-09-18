aefd777605232e1e836448894ca840dd7f85d772
Gennadiy-Zubariev <genada89@gmail.com>
2026-09-18 16:33:53 +0300

    add permissions

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "rest_framework.authtoken",
     "user",
     "app_media",
 ]
@@ -131,6 +132,15 @@ MAILERS = {
     "default": {
         "BACKEND": "django.core.mail.backends.console.EmailBackend",
     },
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 AUTH_USER_MODEL = "user.User"
