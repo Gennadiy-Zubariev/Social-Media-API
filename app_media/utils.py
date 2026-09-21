f2d5b52c23c292010ce2879a6882bfedf597968d
Gennadiy-Zubariev <genada89@gmail.com>
2026-09-21 22:11:51 +0300

    Revert "Social_media_API app"

--- app_media/utils.py
+++
@@ -1,7 +0,0 @@
-import re
-
-HASHTAG_RE = re.compile(r"#(\w+)")
-
-
-def parse_hashtags(content):
-    return HASHTAG_RE.findall(content.lower())
--- social_media_api/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "social_media_api.settings")
-
-app = Celery("social_media_api")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
